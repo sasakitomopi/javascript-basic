d2d63769e25cdd71bec6515cc83695da2d132b41
sasakitomopi <takatobi0326@icloud.com>
2024-05-12 15:44:24 +0900

    feat: 課題014の作成

---
+++ kadai_014/js/square.js
@@ -0,0 +1,13 @@
+const squareValueByArrow = (num) => {
+    console.log(num * num);
+}
+
+function squareValueByFunction(num) {
+    console.log(num * num);
+}
+
+// アロー関数での実行
+squareValueByArrow(2);
+
+// 関数での実行
+squareValueByFunction(2);
